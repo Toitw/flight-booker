12920b470093592e72addb53b4de2344aae65e3b
juroga_new <juanjrgast@gmail.com>
2023-07-03 16:23:56 +0200

    Tried to add date correctly, not working yet

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,5 +1,9 @@
 class FlightsController < ApplicationController
     def index
         @flights = Flight.all
+        @available_dates = []
+        if params[:departure_airport_id] && params[:arrival_airport_id]
+        @available_dates = Flight.available_dates(params[:departure_airport_id], params[:arrival_airport_id])
+        end
     end
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -3,4 +3,13 @@ class Flight < ApplicationRecord
     belongs_to :arrival_airport, class_name: 'Airport'
     validates :departure_airport_id, presence: true
     validates :arrival_airport_id, presence: true
+
+    def event_date_formatted
+        departure_time.strftime("%m/%d/%Y")
+    end
+
+    def self.available_dates(departure_airport_id, arrival_airport_id)
+        where(departure_airport_id: departure_airport_id, arrival_airport_id: arrival_airport_id)
+          .pluck('DISTINCT DATE(start) AS event_date')
+    end
 end
