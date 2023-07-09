41e714fbdb6fa925c80b86474ff1bcaf7ee8d641
juroga_new <juanjrgast@gmail.com>
2023-07-09 10:27:30 +0200

    Changed views and controller to use form_with instead of form_tag

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,10 +1,12 @@
 class FlightsController < ApplicationController
     def index
       @available_dates = Flight.pluck(:start).map(&:to_date).uniq
-  
+      @flight = Flight.new
+
       if params[:flight].present?
-        departure_airport_id = params[:flight][:departure_airport_id]
-        arrival_airport_id = params[:flight][:arrival_airport_id]
+        flight_params = flight_form_params
+        departure_airport_id = flight_params[:departure_airport_id]
+        arrival_airport_id = flight_params[:arrival_airport_id]
   
         if departure_airport_id.present? && arrival_airport_id.present?
           @available_flights = Flight.where(departure_airport_id: departure_airport_id, arrival_airport_id: arrival_airport_id)
@@ -15,7 +17,14 @@ class FlightsController < ApplicationController
         @available_flights = []
       end
     end
+  
+    private
+  
+    def flight_form_params
+      params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :num_passengers, :departure_date)
+    end
   end
+  
   
 
 
