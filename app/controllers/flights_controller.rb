6c45f811e75890a217050899f080f6629f89ec6c
juroga_new <juanjrgast@gmail.com>
2023-07-09 10:42:59 +0200

    Added departure_date to the filter so it only displays the flights on that date

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,9 +7,10 @@ class FlightsController < ApplicationController
         flight_params = flight_form_params
         departure_airport_id = flight_params[:departure_airport_id]
         arrival_airport_id = flight_params[:arrival_airport_id]
+        departure_date = flight_params[:departure_date]
   
         if departure_airport_id.present? && arrival_airport_id.present?
-          @available_flights = Flight.where(departure_airport_id: departure_airport_id, arrival_airport_id: arrival_airport_id)
+          @available_flights = Flight.where(departure_airport_id: departure_airport_id, arrival_airport_id: arrival_airport_id, start: departure_date.to_datetime.beginning_of_day..departure_date.to_datetime.end_of_day)
         else
           @available_flights = []
         end
